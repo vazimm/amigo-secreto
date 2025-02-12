24a2c3b47ddfce7f8e267cc37174c5731f46e246
Luiz Davi Vaz <143763800+vazimm@users.noreply.github.com>
2025-02-11 23:27:26 -0300

    Filtro de Entrada

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -2,15 +2,32 @@
 let array_amigos = [];
 
 function adicionar() {
+    //Atribuição de valor da Entrada a uma variavel
+    let amigos = document.getElementById('nome-amigo').value;
+    //Verifica se o campo está vazio
+    if (amigos === "") {
+        alert("O campo está vazio!")
+        return
+    }
     //Atualiza a lista através da entrada no HTML
-    array_amigos.push(document.getElementById('nome-amigo').value);
+    array_amigos.push(amigos);
+    //Valida se o nome não possui caracteres especiais ou numeros
+    validarCaracteres();
     //Imprime o nome dos participantes na página do sorteio
-    document.getElementById("lista-amigos").textContent = array_amigos.join(", ")
+    document.getElementById("lista-amigos").textContent = array_amigos.join(", ");
     //Limpa a entrada
-    document.getElementById('nome-amigo').value = ''; 
+    document.getElementById('nome-amigo').value = "";
 }
 
 function sortear(){
+    //Caso queiram tirar um novo resultado com os mesmos integrantes 
+    document.getElementById("lista-sorteio").textContent = '';
+    //Caso venha de acontecer de haver participantes insuficientes
+    if (array_amigos.length < 3) {
+        console.log(array_amigos)
+        return alert("O número de participantes não pode ser inferior a 3!");
+    }
+    
     //Função que vai embaralhar o array
     embaralha(array_amigos);
     //Variável que busca o elemento Sorteio para impressão do resultado
@@ -19,7 +36,7 @@ function sortear(){
     for(i = 0; i < array_amigos.length; i++){
         if(i == array_amigos.length - 1){
             //Atualiza a lista com o resutado quando o loop atinge o último item na lista   
-            sorteio.innerHTML += `${array_amigos[i]} --> ${array_amigos[0]} <br>`
+            sorteio.innerHTML += `${array_amigos[i]} --> ${array_amigos[0]} <br>`;
         }  else {
             //Atualiza a lista com o resutado do sorteio
             sorteio.innerHTML += `${array_amigos[i]} --> ${array_amigos[i + 1]} <br>`;
@@ -43,9 +60,22 @@ function embaralha(lista) {
 
 function reiniciar(){
     //Limpa todos os campos
-    document.getElementById("lista-amigos").textContent = ''
-    document.getElementById("lista-sorteio").textContent = ''
+    document.getElementById("lista-amigos").textContent = '';
+    document.getElementById("lista-sorteio").textContent = '';
     document.getElementById('nome-amigo').value = '';
     //Limpar o Array
-    array_amigos = []
+    array_amigos = [];
+}
+
+// Função para validar caracteres
+function validarCaracteres() {
+    // Expressão regular para aceitar apenas letras (maiúsculas e minúsculas)
+    const regex = /^[a-zA-Z ]+$/;
+    if (regex.test(document.getElementById('nome-amigo').value)) {
+    	
+	} else {
+        array_amigos.pop();
+        console.log(array_amigos);
+        alert("Nome não pode conter número ou caracteres especiais")
+    }
 }
